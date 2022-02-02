4f6ea7cda12e6d7dc31748e2d93c32ade1591f73
Daniil Yarockii <yaroc.d@gmail.com>
2022-02-02 11:36:22 +0300

    config added

--- supervisely_app/main.py
+++ supervisely_app/main.py
@@ -1,12 +1,15 @@
 import kitti_360_helpers
-import sly_functions as f
 import pointcloud_episode
 
+import sly_functions as f
+import sly_globals as g
 
 if __name__ == '__main__':
-    kitti_360_helpers.get_kitti_360_data()  # download data from server
-    f.convert_kitti360_to_supervisely_pcl_episodes_project()  # convert kitti to supervisely
-    pointcloud_episode.upload_pcl_episodes_project()  # upload converted project to server
+    kitti_360_helpers.get_kitti_360_data()                      # download data from server
+    f.convert_kitti360_to_supervisely_pcl_episodes_project()    # convert kitti to supervisely
+    pointcloud_episode.upload_pcl_episodes_project()            # upload converted project to server
+
+    g.my_app.stop()
 
 
 
